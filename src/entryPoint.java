0e5825bbffe7e0c81ca3cfe8bf0f709f15e47934
Dallas Mann <Dallas.mann94@gmail.com>
2017-03-25 12:17:29 -0600

    added a message to tell the user how to run the program

--- src/entryPoint.java
+++ src/entryPoint.java
@@ -1,5 +1,11 @@
 public class entryPoint {
 	public static void main(String[] args){
+		
+		if(args.length != 4){
+			System.out.println("Correct usage: java entryPoint <inputImage.png> <mask1.txt> <mask2.txt> <outputImage.png>");
+			System.exit(-1);
+		}
+		
 		Operator myOperator = new Operator();
 		Image inputImage = new Image(args[0]);
 		Mask maskHorizontal = new Mask(args[1]);
